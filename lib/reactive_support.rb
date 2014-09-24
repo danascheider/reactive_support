8f658f8ddb6ac2107e160e4e091190da0746b502
Dana Scheider <dana.scheider@gmail.com>
2014-09-24 13:58:20 -0700

    Add comment for Inch

--- lib/reactive_support.rb
+++ lib/reactive_support.rb
@@ -8,6 +8,9 @@ module ReactiveSupport
   end
 end
 
+# Include ReactiveSupport in Ruby's +Object+ class. This enables ReactiveSupport
+# methods to be called on any Ruby object without explicit inclusion.
+
 class Object
   include ReactiveSupport
 end
