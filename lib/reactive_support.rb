0a59ab0bbf791bcff89f00f68d9a1055db4fe882
Dana Scheider <dana.scheider@gmail.com>
2014-09-24 13:25:07 -0700

    Add lib and spec files

---
+++ lib/reactive_support.rb
@@ -0,0 +1,2 @@
+module ReactiveSupport
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require 'simplecov'
+require 'coveralls'
+require 'rspec'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start if ENV["COVERAGE"]
+
+require_relative '../lib/reactive_support'
+
+RSpec.configure do |c|
+  c.order = 'random'
+end
