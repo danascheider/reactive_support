bed46ca8df19b9b683c76216822e781229fd9ff6
Dana Scheider <dana.scheider@gmail.com>
2014-09-25 23:02:31 -0700

    Define #in? method

--- lib/reactive_support.rb
+++ lib/reactive_support.rb
@@ -46,6 +46,43 @@ module ReactiveSupport
 
   alias_method :exist?, :exists?
 
+  # The +#in?+ method returns +true+ if the calling object is included in the
+  # +object+ passed as a parameter. The +object+ parameter must be a String,
+  # Enumerable, or other object that responds to +#include?+. If it doesn't, 
+  # the #in? method will raise an ArgumentError.
+  #
+  # When passed an Array object, +#in?+ returns +true+ if the calling object
+  # is included as a member of the array:
+  # 
+  #     'foo'.in? ['foo', 'bar']    # => true
+  #     'foo'.in? ['bar', 'baz']    # => false
+  #
+  # When passed a Hash object, +#in?+ returns true if the calling object is
+  # included as a key in the hash. To look for an object in a hash's values,
+  # use the hash's +#values+ method.
+  #
+  #     'foo'.in? {'foo' => 'bar'}          # => true 
+  #     'foo'.in? {'bar' => 'foo'}          # => false
+  #     'foo'.in? {'bar' => 'foo'}.values   # => true 
+  # 
+  # When passed a String object, +#in?+ returns +true+ if the the calling object
+  # (which must also be a string) appears verbatim within the parameter object.
+  # If the passed-in object is a string but the calling object is something else,
+  # a TypeError will be returned.
+  #
+  #     'foo'.in? 'foobar'    # => true
+  #     'foo'.in? 'foto'      # => false
+  #     ['foo'].in? 'foobar'  # => TypeError
+  #
+  # When passed an object that does not respond to +#include?+, +#in?+ raises
+  # an ArgumentError.
+
+  def in?(object)
+    object.include?(self)
+  rescue NoMethodError
+    raise ArgumentError.new("The parameter passed to #in? must respond to #include?")
+  end
+
   # The +#try+ method calls the given +method+ (with given +*args+ and +&block+)
   # on the object calling it. The +#try+ method returns the output of the 
   # method or, if an error is raised, +nil+. It accepts an arbitrary number 
@@ -67,12 +104,6 @@ module ReactiveSupport
   # Examples of a method being called with a block:
   #     { foo: 10, bar: 18, baz: 32 }.try(:reject!) {|k,v| v < 25 } # => { baz: 32 }
   #     nil.try(:reject) {|k,v| v == 'foo' }                        # => nil
-
-  def in?(object)
-    object.include?(self)
-  rescue NoMethodError
-    raise ArgumentError.new("The parameter passed to #in? must respond to #include?")
-  end
 
   def try(method, *args, &block)
     begin
