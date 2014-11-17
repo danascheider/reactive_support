c9d9239132683c3c04c939c11b40f862efd3c40b
Dana Scheider <dana.scheider@gmail.com>
2014-11-17 14:15:14 -0800

    Fix problem with requires

--- version.rb
+++ version.rb
@@ -6,7 +6,7 @@ module ReactiveSupport
   module Version
     MAJOR = '0'
     MINOR = '2'
-    PATCH = '0'
+    PATCH = '1'
     PRE   = ''
 
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.').chomp('.')
