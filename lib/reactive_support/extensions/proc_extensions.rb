d72d0914e60ee52008c1c35a81bb9ddc778622e8
Dana Scheider <dana.scheider@gmail.com>
2014-11-25 12:09:33 -0800

    Finish Proc#raises_error? method

--- lib/reactive_support/extensions/proc_extensions.rb
+++ lib/reactive_support/extensions/proc_extensions.rb
@@ -4,7 +4,26 @@
 # 1.9.3[http://ruby-doc.org/core-1.9.3/Proc.html].
 
 class Proc
+
+  # The +#raises_error?+ method checks whether an exception is raised
+  # when the calling Proc is called with the given +*args+.
+  #
+  # The +#raises_error?+ method does not actually create lasting changes
+  # to objects or variables; it only checks whether an exception would be
+  # raised if the Proc were called with the given +*args+.
+  #
+  # Basic examples:
+  #     proc = Proc.new {|q| 1.quo(q) }
+  #     proc.raises_error?(2)       # => false
+  #     proc.raises_error?(0)       # => true
+  #
+  # Examples with destructive proc:
+  #     hash = {:foo => :bar}
+  #     proc = Proc.new {|hash| hash.reject! {|k,v| k === :foo } }
+  #     proc.raises_error?(hash)    # => false
+  #     hash                        # => {:foo => :bar}
+
   def raises_error?(*args)
-    (!self.call(*args)) rescue true
+    (!self.call(*args.deep_dup)) rescue true
   end
 end
--- spec/reactive_extensions_spec.rb
+++ spec/reactive_extensions_spec.rb
@@ -147,11 +147,20 @@ describe ReactiveExtensions do
         it 'returns true' do 
           expect(proc.raises_error?(0)).to be true
         end
+
+        it 'handles the exception' do 
+          expect{ proc.raises_error?(0) }.not_to raise_error
+        end
       end
 
       context 'when no error is raised' do 
         it 'returns false' do 
           expect(proc.raises_error?(2)).to be false
+        end
+
+        it 'doesn\'t change objects' do 
+          h, p = {:foo => :bar}, Proc.new {|hash| hash.reject! {|k,v| k === :foo } }
+          expect{ p.raises_error?(h) }.not_to change(h, :length)
         end
       end
     end
