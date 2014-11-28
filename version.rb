c19f70402322f83db170404837c32581604d415a
Dana Scheider <dana.scheider@gmail.com>
2014-11-27 22:12:05 -0800

    Update to 0.4.0.beta2

--- version.rb
+++ version.rb
@@ -7,7 +7,7 @@ module ReactiveSupport
     MAJOR = '0'
     MINOR = '4'
     PATCH = '0'
-    PRE   = 'beta'
+    PRE   = 'beta2'
 
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.').chomp('.')
   end
