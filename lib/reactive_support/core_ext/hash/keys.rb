84ffd72c90a143baec519caaf9ec8d1eee63519a
Dana Scheider <dana.scheider@gmail.com>
2014-11-29 13:58:48 -0800

    Add #assert_valid_keys method

--- lib/reactive_support/core_ext/hash/keys.rb
+++ lib/reactive_support/core_ext/hash/keys.rb
@@ -1,8 +1,109 @@
-# This file adds the +#transform_keys+ and +#transform_keys!+ methods
-# to Ruby's core +Hash+ class. These methods iterate through a hash's
-# keys, passing each to a block and changing them accordingly.
+# This file adds the +#transform_keys+, +#transform_keys!+,
+# +#stringify_keys+, +#stringify_keys!+, +#symbolize_keys+, and 
+# +#symbolize_keys! methods to Ruby's core +Hash+ class. These
+# methods modify the hash's keys according to given parameters,
+# or create a duplicate with keys modified in this way.
+
+# Ruby's core Hash class. See documentation for version 
+# 2.1.5[http://ruby-doc.org/core-2.1.5/Hash.html],
+# 2.0.0[http://ruby-doc.org/core-2.0.0/Hash.html], or 
+# 1.9.3[http://ruby-doc.org/core-1.9.3/Hash.html].
 
 class Hash
+
+  # The +#assert_valid_keys method validates that all keys in a hash 
+  # match +*valid_keys+, raising an ArgumentError if the keys don't
+  # match. Note that, as usual, symbols and strings are treated as 
+  # distinct.
+  #
+  # Examples:
+  #     {:foo => 'bar'}.assert_valid_keys(:foo)         # => true
+  #     {:foo => 'bar'}.assert_valid_keys(:foo, :baz)   # => true
+  #     {:foo => 'bar'}.assert_valid_keys(:baz)         # => ArgumentError
+  #     {:foo => 'bar'}.assert_valid_keys('foo')        # => ArgumentError
+
+  def assert_valid_keys(*valid_keys)
+    valid_keys.flatten!
+    each_key do |key|
+      unless valid_keys.include?(key) 
+        raise ArgumentError.new("Unknown key: #{key.inspect}. Valid keys are: #{valid_keys.map(&:inspect).join(', ')}")
+      end
+    end
+  end
+
+  # The +#stringify_keys+ method returns a hash identical to the calling
+  # hash, but with symbol keys turned into strings. It is non-destructive;
+  # the original hash is still available after it is called.
+  #
+  # Although this method was formerly a part of ActiveSupport, it was
+  # already deprected by the time ReactiveSupport was introduced. For
+  # that reason, it is being included as part of ReactiveExtensions.
+  #
+  # Examples: 
+  #     orig = { :foo => 'bar' }
+  #     dup = orig.stringify_keys
+  # 
+  #     orig      #=> { :foo => 'bar' }
+  #     dup       #=> { 'foo' => 'bar' }
+
+  def stringify_keys
+    transform_keys &:to_s
+  end
+
+  # The +#stringify_keys!+ method converts symbol hash keys into strings.
+  # It is a destructive method; the original hash is changed when this
+  # method is called.
+  #
+  # Although this method was formerly a part of ActiveSupport, it was already
+  # deprecated by the time ReactiveSupport was introduced. For that reason,
+  # it is being included as part of ReactiveExtensions.
+  #
+  # Examples:
+  #     orig = { :foo => 'bar' }
+  #     orig.symbolize_keys!
+  #
+  #     orig      #=> { 'foo' => 'bar' }
+
+  def stringify_keys!
+    transform_keys! &:to_s
+  end
+
+  # The +#symbolize_keys+ method returns a hash identical to the calling
+  # hash but with string keys turned into symbols. It is non-destructive;
+  # the original hash is still available after it is called.
+  #
+  # Although this method was formerly a part of ActiveSupport, it was 
+  # already deprecated by the time ReactiveSupport was introduced. For
+  # that reason, it is being included as part of ReactiveExtensions.
+  #
+  # Examples:
+  #     orig = { 'foo' => 'bar' }
+  #     dup = orig.symbolize_keys
+  #
+  #     orig      #=> { 'foo' => 'bar' }
+  #     dup       #=> { :foo => 'bar' }
+
+  def symbolize_keys
+    transform_keys {|key| key.to_sym }
+  end
+
+  # The +#symbolize_keys!+ method converts string hash keys into symbols.
+  # It is a destructive method; the original hash is changed when this
+  # method is called.
+  #
+  # Although this method was formerly a part of ActiveSupport, it was already
+  # deprecated by the time ReactiveSupport was introduced. For that reason,
+  # it is being included as part of ReactiveExtensions.
+  #
+  # Examples:
+  #     orig = { 'foo' => 'bar' }
+  #     orig.symbolize_keys!
+  #
+  #     orig      #=> { :foo => 'bar' }
+
+  def symbolize_keys!
+    transform_keys! {|key| key.to_sym }
+  end
 
   # The +#transform_keys+ method returns a new hash with all keys transformed
   # in accordance with the given +block+. The +#transform_keys+ method is 
--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -3,6 +3,84 @@ require 'spec_helper'
 describe Hash do 
   let(:hash) { { :foo => 'bar', :baz => 'qux' } }
 
+  describe 'assert_valid_keys' do 
+    context 'all keys are valid' do 
+      it 'returns the hash' do 
+        expect(hash.assert_valid_keys(:foo, :baz)).to eql hash
+      end
+
+      it 'doesn\'t raise an error' do 
+        expect{ hash.assert_valid_keys(:foo, :baz) }.not_to raise_error
+      end
+    end
+
+    context 'keys are a subset of valid keys' do 
+      it 'returns the hash' do 
+        expect(hash.assert_valid_keys(:foo, :bar, :baz)).to eql hash
+      end
+
+      it 'doesn\'t raise an error' do 
+        expect{ hash.assert_valid_keys(:foo, :bar, :baz) }.not_to raise_error
+      end
+    end
+
+    context 'keys are wrong type' do 
+      it 'raises an ArgumentError' do 
+        expect{ hash.assert_valid_keys(:foo, 'baz') }.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'invalid keys are present' do 
+      it 'raises an ArgumentError' do 
+        expect{ hash.assert_valid_keys(:foo, 'baz') }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe 'stringify_keys' do 
+    it 'turns symbol keys into strings' do 
+      expect(hash.stringify_keys).to eql({'foo' => 'bar', 'baz' => 'qux'})
+    end
+
+    it 'maintains the original hash' do 
+      expect { hash.stringify_keys }.not_to change(hash, :keys)
+    end
+  end
+
+  describe 'stringify_keys!' do 
+    it 'turns symbol keys into strings' do 
+      expect(hash.stringify_keys!).to eql({'foo' => 'bar', 'baz' => 'qux'})
+    end
+
+    it 'overwrites the original hash' do
+      expect { hash.stringify_keys! }.to change(hash, :keys)
+    end
+  end
+
+  describe 'symbolize_keys' do 
+    let(:hash) { {'foo' => 'bar' } }
+
+    it 'turns string keys into symbols' do 
+      expect(hash.symbolize_keys).to eql({:foo => 'bar'})
+    end
+
+    it 'maintains the original hash' do 
+      expect { hash.symbolize_keys }.not_to change(hash, :keys)
+    end
+  end
+
+  describe 'symbolize_keys!' do 
+    let(:hash) { {'foo' => 'bar' } }
+
+    it 'turns string keys into symbols' do 
+      expect(hash.symbolize_keys!).to eql({:foo => 'bar'})
+    end
+
+    it 'overwrites the original hash' do
+      expect{ hash.symbolize_keys! }.to change(hash, :keys)
+    end
+  end
+
   describe 'transform_keys' do  
     it 'changes keys according to a block' do 
       transformed = hash.transform_keys {|key| key.to_s.upcase }
