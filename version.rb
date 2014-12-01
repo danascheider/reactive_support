1e943bc2b1b080d5b52cb5074a4b18401423194d
Dana Scheider <dana.scheider@gmail.com>
2014-11-30 17:51:16 -0800

    Version 0.5.0 stable

--- version.rb
+++ version.rb
@@ -7,7 +7,7 @@ module ReactiveSupport
     MAJOR = '0'
     MINOR = '5'
     PATCH = '0'
-    PRE   = 'beta2'
+    PRE   = ''
 
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.').chomp('.')
   end
